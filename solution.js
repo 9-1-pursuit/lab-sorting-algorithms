297605a6fd1c479a0729717983cb705f23cd8b3e
minagrullon <minagrullon@pursuit.org>
2023-04-03 13:59:55 -0400

    passing 9 out of 11

--- solution.js
+++ solution.js
@@ -1,34 +1,76 @@
 const { catArt, someNums, someProducts, someWords } = require("./data/data.js");
 
 // sort numbers in ascending order
-const sortNumsA = () => {};
+const sortNumsA = (someNums) => {
+  return someNums.sort((a, b) => a - b);
+};
 
 // sort numbers in descending order
-const sortNumsD = () => {};
+const sortNumsD = (someNums) => {
+  return someNums.sort((a, b) => b - a);
+};
 
 // sort words in ascending order case sensitive
-const sortWordsA = () => {};
+const sortWordsA = (someWords) => {
+  return someWords.sort();
+};
 
 // sort words in descending order case insensitive
-const sortWordsD = () => {};
+const sortWordsD = (someWords) => {
+  return someWords.sort((a, b) =>
+    b.toLowerCase().localeCompare(a.toLowerCase())
+  );
+};
 
 // sort products by name, ascending order case insensitive
-const sortProductNamesA = () => {};
+const sortProductNamesA = (someProducts) => {
+  return someProducts.sort((a, b) =>
+    a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+  );
+};
 
 // sort products by price, ascending order
-const sortProductPriceA = () => {};
+const sortProductPriceA = (someProducts) => {
+  return someProducts.sort((a, b) => a.price - b.price);
+};
 
 // sort products by price, descending order
-const sortProductPriceD = () => {};
+const sortProductPriceD = (someProducts) => {
+  return someProducts.sort((a, b) => b.price - a.price);
+};
 
 // sort products by price, then by name, ascending order
-const sortProducsPriceNameA = () => {};
+const sortProducsPriceNameA = (someProducts) => {
+  return someProducts.sort((a, b) => a.price - b.price).sort();
+};
 
 // sort catArt by designed by
-const catArtSortDesginedByA = () => {};
+const catArtSortDesginedByA = (catArt) => {
+  return catArt.sort((a, b) =>
+    a.designedBy.toLowerCase().localeCompare(b.designedBy.toLowerCase())
+  );
+};
 
 // sort catArt by price
-const catArtSortByPriceA = () => {};
+const catArtSortByPriceA = (catArt) => {
+  // return catArt.sort((a, b) => {
+  //   //! check for pluto char but not changing the actual data
+  //   // may still need to use another method besides sort
+  //   // within a conditional, how can you do this. can make a temp conversion
+  //   // use those tmep variables to compare for sort
+  //   // Remove ♇♇ from prices and convert them to numbers
+  //   const priceA = Number(a.price.toString().replace(/♇♇/g, ""));
+  //   const priceB = Number(b.price.toString().replace(/♇♇/g, ""));
+
+  //   // Sort by price
+  //   return priceA - priceB;
+  // });
+  return catArt.slice().sort((a, b) => {
+    const priceA = Number(a.price.toString().replace(/♇♇/g, ""));
+    const priceB = Number(b.price.toString().replace(/♇♇/g, ""));
+    return priceA - priceB;
+  });
+};
 
 // Create your own sort function
 // it should sort in ascending order
