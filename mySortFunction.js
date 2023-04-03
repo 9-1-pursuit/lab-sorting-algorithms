4515b980b5cb8ed49d2378d4e0e97d20dc00406a
DestinyJoyner <destinyjoyner@pursuit.org>
2023-04-03 17:29:54 -0400

    my sort sorts numbers/ strings ascending case sensitive

--- mySortFunction.js
+++ mySortFunction.js
@@ -26,16 +26,45 @@
 */
 
 const array = [9,-3,5,2,6,8,-6,1,3]
+const someWords = [
+    "The",
+    "quick",
+    "brown",
+    "fox",
+    "jumps",
+    "over",
+    "the",
+    "lazy",
+    "dog",
+  ];
 
 function mySort(arr) {
     if(arr.length <= 1){
         return arr
     }
     // value to be compared to other values in array, will then be 'centered in return array
-   const middle= arr[0]
+   let middle = arr[0]
    const [left, right] = arr.reduce((acc, el, index) => {
     // index > 0 since compare value is index 0 start comparing at index 1
     if(index > 0){
+        // differentiate b/w numbers and strings
+        if(isNaN(el)){
+            if(el[0] === middle[0]){
+                let index = 0
+                let letterA 
+                let letterB 
+                while(letterA === letterB){
+                  letterA = el[index]
+                  letterB = middle[index]
+                  index++
+                }
+                if(letterA.toLowerCase().charCodeAt(0) > letterB.toLowerCase().charCodeAt(0)){
+                    el = 1
+                    middle = 0
+                }
+            }
+           
+        }
         const accIndex = el > middle ? 1 : 0
         acc[accIndex].push(el)
     }
@@ -49,5 +78,5 @@ function mySort(arr) {
 //    console.log(mySort(left))
 //    console.log(mySort(right))
 }
-
+console.log("T".charCodeAt(0), "t".charCodeAt(0))
 console.log(mySort(array))
