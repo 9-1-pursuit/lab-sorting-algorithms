a830781e84a0c4e8b278161b190c4d490d72513d
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2023-04-04 22:04:52 +0530

    passing all

--- solution.js
+++ solution.js
@@ -1,34 +1,68 @@
-const { catArt, someNums, someProducts, someWords } = require("./data/data.js");
+const { catArt, someNums, someProducts, someWords } = require('./data/data.js');
 
 // sort numbers in ascending order
-const sortNumsA = () => {};
+const sortNumsA = (nums) => {
+  return nums.sort((a, b) => a - b);
+};
 
 // sort numbers in descending order
-const sortNumsD = () => {};
+const sortNumsD = (nums) => {
+  return nums.sort((a, b) => b - a);
+};
 
 // sort words in ascending order case sensitive
-const sortWordsA = () => {};
+const sortWordsA = (word) => {
+  return word.sort();
+};
 
 // sort words in descending order case insensitive
-const sortWordsD = () => {};
+const sortWordsD = (word) => {
+  return word.sort((a, b) => b.localeCompare(a));
+};
 
 // sort products by name, ascending order case insensitive
-const sortProductNamesA = () => {};
+const sortProductNamesA = (product) => {
+  return product.sort((a, b) => a.name.localeCompare(b.name));
+};
 
 // sort products by price, ascending order
-const sortProductPriceA = () => {};
+const sortProductPriceA = (price) => {
+  return price.sort((a, b) => a.price - b.price);
+};
 
 // sort products by price, descending order
-const sortProductPriceD = () => {};
+const sortProductPriceD = (price) => {
+  return price.sort((a, b) => b.price - a.price);
+};
 
 // sort products by price, then by name, ascending order
-const sortProducsPriceNameA = () => {};
+const sortProducsPriceNameA = (someProducts) => {
+  return someProducts.sort((a, b) => a.price - b.price).sort();
+};
 
 // sort catArt by designed by
-const catArtSortDesginedByA = () => {};
+const catArtSortDesginedByA = (art) => {
+  return art.sort((a, b) => a.designedBy.localeCompare(b.designedBy));
+};
 
 // sort catArt by price
-const catArtSortByPriceA = () => {};
+const catArtSortByPriceA = (price) => {
+  return price.sort((a, b) => {
+    let price1;
+    let price2;
+    if (typeof a.price === 'string' && a.price.includes('♇♇')) {
+      price1 = a.price.slice(2) * 10;
+    } else {
+      price1 = Number(a.price);
+    }
+    if (typeof b.price === 'string' && b.price.includes('♇♇')) {
+      price2 = b.price.slice(2) * 10;
+    } else {
+      price2 = Number(b.price);
+    }
+    return price1 - price2;
+  });
+};
 
 // Create your own sort function
 // it should sort in ascending order
@@ -38,7 +72,23 @@ const catArtSortByPriceA = () => {};
 // or try to implement merge sort
 // or look up another common sort algorithm (i.e. quicksort, ) and try your own implementation
 // Bonus add another argument that would allow the function to be used for ascending or descending order
-const mySortFunction = () => {};
+const mySortFunction = (arr) => {
+  let swap;
+
+  for (let i = arr.length; i > 0; i--) {
+    swap = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+        swap = false;
+      }
+    }
+    if (swap) break;
+  }
+  return arr;
+};
+
+console.log(mySortFunction(someNums));
 
 module.exports = {
   sortNumsA,
